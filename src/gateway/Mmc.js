51d1a1f9ba304a09e4df6340204aebec504a9e12
giorgi <giorgi.bibilashvili89@gmail.com>
2018-10-26 09:07:45 +0400

    --

--- src/gateway/Mmc.js
+++ src/gateway/Mmc.js
@@ -4,11 +4,29 @@ class Mmc {
 
     login = (grantType, username, password, clientId, clientSecret) => {
 
-        console.log(username);
-        console.log(password);
-        console.log(grantType);
-        console.log(clientId);
-        console.log(clientSecret);
+        let post = {
+            username,
+            password,
+            grant_type: grantType,
+            client_id: clientId,
+            client_secret: clientSecret,
+        }
+
+        console.log(post);
+
+        fetch('http://haccp.milady.io/oauth/token', {
+            method: "POST",
+            body: new FormData(post),
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+            },
+        }).then(function (response) {
+
+            console.log(response.text());
+
+        }, function (error) {
+            console.log(error.message); //=> String
+        })
 
     }
 
